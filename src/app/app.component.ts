123365ab61c8c350834e3995bd67dd97f2d67f7f
Jason White <jmw5598@gmail.com>
2018-09-13 19:39:29 -0400

    update app module, app component and app template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { AccountService } from './core/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit, OnDestroy {
+
+  private subscription: Subscription;
+  public user;
+
+  constructor(
+    public accountService: AccountService) {}
+
+  isAccountSetupComplete() {
+    return true;
+  }
+
+  ngOnInit() {
+    this.subscription = this.accountService.user.subscribe(
+      user => this.user = user
+    );
+  }
+
+  ngOnDestroy() {
+    if(this.subscription)
+      this.subscription.unsubscribe();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,56 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
+import { AccountEditComponent } from './account/account-edit/account-edit.component';
+import { AccountSetupComponent } from './account/account-setup/account-setup.component';
+import { AccountSummaryComponent } from './account/account-summary/account-summary.component';
+
+import { AccountService } from './core/account.service';
+import { AccountSetupCompleteGuard } from './core/account-setup-complete.guard';
+
+const appRoutes: Routes = [
+  {
+    path: 'accountedit',
+    canActivate: [AccountSetupCompleteGuard],
+    component: AccountEditComponent
+  },
+  {
+    path: 'accountsetup',
+    component: AccountSetupComponent
+  },
+  {
+    path: 'accountsummary',
+    canActivate: [AccountSetupCompleteGuard],
+    component: AccountSummaryComponent
+  },
+  {
+    path: '',
+    redirectTo: 'accountsummary',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'accountsummary',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountEditComponent,
+    AccountSetupComponent,
+    AccountSummaryComponent,
+    NavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { useHash: true })
   ],
-  providers: [],
+  providers: [AccountService, AccountSetupCompleteGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
