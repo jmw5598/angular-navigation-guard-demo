7c6d7818505723ca59ee1d46473c25cfdab0f9c2
Jason White <jmw5598@gmail.com>
2018-09-14 13:03:45 -0400

    moved logic to hide/show nav to navigation component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,12 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs/Subscription';
-import { AccountService } from './core/account.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent {
 
-  private subscription: Subscription;
-  public user;
-
-  constructor(
-    public accountService: AccountService) {}
-
-  isAccountSetupComplete() {
-    return true;
-  }
-
-  ngOnInit() {
-    this.subscription = this.accountService.user.subscribe(
-      user => this.user = user
-    );
-  }
-
-  ngOnDestroy() {
-    if(this.subscription)
-      this.subscription.unsubscribe();
-  }
+  constructor() {}
 
 }
